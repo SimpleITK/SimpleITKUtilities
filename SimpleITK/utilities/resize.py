4046a36e0e662e8f020f451a274a98ff6db33e4e
Bradley Lowekamp <blowekamp@mail.nih.gov>
2023-06-05 08:45:52 -0400

    Add antialias filtering by recursive Gaussian to resize

--- SimpleITK/utilities/resize.py
+++ SimpleITK/utilities/resize.py
@@ -2,6 +2,13 @@ import SimpleITK as sitk
 from typing import Sequence
 from typing import Union
 from math import ceil
+import collections
+
+
+def _issequence(obj):
+    if isinstance(obj, (bytes, str)):
+        return False
+    return isinstance(obj, collections.abc.Sequence)
 
 
 def resize(
@@ -10,8 +17,9 @@ def resize(
     isotropic: bool = True,
     fill: bool = True,
     interpolator=sitk.sitkLinear,
-    outside_pixel_value: Union[int, float] = 0,
-    use_nearest_extrapolator=False,
+    fill_value: float = 0.0,
+    use_nearest_extrapolator: bool = False,
+    anti_aliasing_sigma: Union[None, float, Sequence[float]] = None,
 ) -> sitk.Image:
     """
     Resize an image to an arbitrary size while retaining the original image's spatial location.
@@ -28,10 +36,14 @@ def resize(
     with constant or nearest values used to fill in the new image. If False and isotropic is True, the output image's
     new size will be calculated to fit the original image's extent such that at least one dimension is equal to
     new_size.
-    :param outside_pixel_value: Value used for padding.
+    :param fill_value: Value used for padding.
     :param interpolator: Interpolator used for resampling.
-    :param use_nearest_extrapolator: If True, use a nearest neighbor extrapolator when resampling, instead of
+    :param use_nearest_extrapolator: If True, use a nearest neighbor for extrapolation when resampling, overridding the
     constant fill value.
+    :param anti_aliasing_sigma: If zero no antialiasing is performed. If a scalar, it is used as the sigma value in
+     physical units for all axes. If None or a sequence, the sigma value for each axis is calculated as
+     $sigma = (new_spacing - old_spacing) / 2$ in physical units. Gaussian smoothing is performed prior to resampling
+     for antialiasing.
     :return: A SimpleITK image with desired size.
     """
     new_spacing = [
@@ -59,13 +71,40 @@ def resize(
 
     new_origin = image.TransformContinuousIndexToPhysicalPoint(new_origin_cidx)
 
+    input_pixel_type = image.GetPixelID()
+
+    if anti_aliasing_sigma is None:
+        # (s-1)/2.0 is the standard deviation of the Gaussian kernel in index space, where s downsample factor defined
+        # by nspc/ospc.
+        anti_aliasing_sigma = [
+            max((nspc - ospc) / 2.0, 0.0)
+            for ospc, nspc in zip(image.GetSpacing(), new_spacing)
+        ]
+    elif not _issequence(anti_aliasing_sigma):
+        anti_aliasing_sigma = [anti_aliasing_sigma] * image.GetDimension()
+
+    if any([s < 0.0 for s in anti_aliasing_sigma]):
+        raise ValueError("anti_aliasing_sigma must be positive, or None.")
+    if len(anti_aliasing_sigma) != image.GetDimension():
+        raise ValueError(
+            "anti_aliasing_sigma must be a scalar or a sequence of length equal to the image dimension."
+        )
+
+    if all([s > 0.0 for s in anti_aliasing_sigma]):
+        image = sitk.SmoothingRecursiveGaussian(image, anti_aliasing_sigma)
+    else:
+        for d, s in enumerate(anti_aliasing_sigma):
+            if s > 0.0:
+                image = sitk.RecursiveGaussian(image, sigma=s, direction=d)
+
     return sitk.Resample(
         image,
         size=new_size,
         outputOrigin=new_origin,
         outputSpacing=new_spacing,
         outputDirection=image.GetDirection(),
-        defaultPixelValue=outside_pixel_value,
+        defaultPixelValue=fill_value,
         interpolator=interpolator,
         useNearestNeighborExtrapolator=use_nearest_extrapolator,
+        outputPixelType=input_pixel_type,
     )
